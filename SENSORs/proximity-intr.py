949f7512b442df55dd0ac4798f1f91de7f65b747
Ranjit W <ran27jit@gmail.com>
2016-01-01 19:16:18 +0000

    RPi: SENSOR: add support for two sensors
    With this patch it is possible to work with two proximity
    sensors.
    
    Signed-off-by: Ranjit W <ran27jit@gmail.com>

--- SENSORs/proximity-intr.py
+++ SENSORs/proximity-intr.py
@@ -2,18 +2,27 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-PIR_PIN = 7
-GPIO.setup(PIR_PIN, GPIO.IN)
 
-def MOTION(PIR_PIN):
-	print "Object Detected!"
+PIR_PIN1 = 7
+PIR_PIN2 = 2
+
+GPIO.setup(PIR_PIN1, GPIO.IN)
+GPIO.setup(PIR_PIN2, GPIO.IN)
+
+def MOTION1(PIR_PIN1):
+	print "Sensor 1 activated!"
+
+def MOTION2(PIR_PIN2):
+	print "Sensor 2 activated!"
 
 print "PIR Module Test (CTRL+C to exit)"
 time.sleep(2)
 print "READY"
 
 try:
-	GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
+	GPIO.add_event_detect(PIR_PIN1, GPIO.RISING, callback=MOTION1)
+	GPIO.add_event_detect(PIR_PIN2, GPIO.RISING, callback=MOTION2)
+	
 	while 1:
 		time.sleep(100)
 
