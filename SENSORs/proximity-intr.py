c3a86e5eb277a5ed7e3ab73054c6119ddb7e2e64
Ranjit W <ran27jit@gmail.com>
2016-01-10 17:05:04 +0000

    SENSOR: update callback method for PIN2
    This patch updates the way of handling the interrupt generated
    via GPIO_PIN2
    
    Signed-off-by: Ranjit W <ran27jit@gmail.com>

--- SENSORs/proximity-intr.py
+++ SENSORs/proximity-intr.py
@@ -7,21 +7,28 @@ PIR_PIN1 = 7
 PIR_PIN2 = 2
 
 GPIO.setup(PIR_PIN1, GPIO.IN)
+#GPIO.setup(PIR_PIN2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(PIR_PIN2, GPIO.IN)
+time_stamp = time.time()
 
 def MOTION1(PIR_PIN1):
 	print "Sensor 1 activated!"
 
 def MOTION2(PIR_PIN2):
-	print "Sensor 2 activated!"
+	global time_stamp
+	time_now = time.time()
+	if (time_now - time_stamp) >= 1:
+		print "Sensor 2 activated"
+	time_stamp = time_now
 
 print "PIR Module Test (CTRL+C to exit)"
 time.sleep(2)
 print "READY"
 
+GPIO.add_event_detect(PIR_PIN2, GPIO.RISING, callback=MOTION2)
 try:
 	GPIO.add_event_detect(PIR_PIN1, GPIO.RISING, callback=MOTION1)
-	GPIO.add_event_detect(PIR_PIN2, GPIO.RISING, callback=MOTION2)
+#	GPIO.add_event_detect(PIR_PIN2, GPIO.RISING, callback=MOTION2)
 	
 	while 1:
 		time.sleep(100)
@@ -30,4 +37,4 @@ except KeyboardInterrupt:
 	print "QUIT"
 	GPIO.cleanup()
 
-
+GPIO.cleanup()
